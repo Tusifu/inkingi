22296d7264ae12104e22d8c779910dfae3e1243f
tusifu <tusifuedison@gmail.com>
2025-06-12 14:06:02 +0200

    update colors when there is lose

--- lib/components/TAppBar.dart
+++ lib/components/TAppBar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:inkingi/constants/colors.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
